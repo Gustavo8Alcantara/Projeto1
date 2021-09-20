b233a1920076aa52e58071f3cb7e6b935f09a63b
Gustavo Alcantara <gustavoalcantara1996@hotmail.com>
2021-09-20 00:08:44 -0300

    primeiro commit

---
+++ js/owl/main.js
@@ -0,0 +1,7 @@
+function redirecionar(){
+    window.open("https://www.youtube.com/watch?v=JGjNoPtlmv0")
+}
+
+function informacao(){
+    window.open("https://www.chevrolet.com/performance/corvette")
+}
